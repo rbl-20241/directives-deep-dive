bd21e1ef46d8950a0d745dc2706e264e3fed0990
rbl-20241 <blom@casema.nl>
2025-09-29 11:46:11 +0200

    156

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -8,6 +8,9 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirective {
+    queryParam = input('myapp', {alias: 'appSafeLink'});
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
     constructor() {
         console.log('SafeLinkDirective is active!');
     }
@@ -16,6 +19,8 @@ export class SafeLinkDirective {
         const wantsToLeave = window.confirm('Do you really really want to leave the app?');
 
         if (wantsToLeave) {
+            const address = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
             return;
         }
 
