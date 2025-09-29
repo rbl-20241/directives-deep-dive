1c8fc4049d4583136978afcdf462f39987febb9c
rbl-20241 <blom@casema.nl>
2025-09-29 11:25:35 +0200

    154

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,25 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+    selector: 'a[appSafeLink]',
+    standalone: true,
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
+})
+export class SafeLinkDirective {
+    constructor() {
+        console.log('SafeLinkDirective is active!');
+    }
+
+    onConfirmLeavePage(event: MouseEvent) {
+        const wantsToLeave = window.confirm('Do you really really want to leave the app?');
+
+        if (wantsToLeave) {
+            return;
+        }
+
+        event.preventDefault();
+    }
+
+}
